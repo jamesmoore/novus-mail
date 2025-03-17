b5317308039d73229453390d01ae389dc93b68f1
jamesmoore <jamesmoore@whatever.com>
2025-03-17 14:55:36 +0000

    use client secret basic

--- api/auth/passport-config.ts
+++ api/auth/passport-config.ts
@@ -1,7 +1,7 @@
 /**
  * See https://github.com/panva/openid-client/blob/main/examples/passport.ts
  */
-import { Configuration, discovery, fetchUserInfo } from 'openid-client'
+import { ClientSecretBasic, Configuration, discovery, fetchUserInfo } from 'openid-client'
 import { Strategy, StrategyOptionsWithRequest, VerifyFunctionWithRequest } from './openid-client-passport.js';
 import { Strategy as AnonymousStrategy } from 'passport-anonymous';
 import { env } from '../env/env.js';
@@ -26,7 +26,8 @@ console.log(oidcEnabled ? 'OIDC enabled' : 'OIDC disabled');
 export const configuration: Configuration = oidcEnabled ? await discovery(
   new URL(authConfig.issuer!), // visit authConfig.issuer + '/.well-known/openid-configuration'
   authConfig.clientID!,
-  authConfig.clientSecret
+  authConfig.clientSecret,
+  ClientSecretBasic(authConfig.clientSecret!)
 ) : {} as Configuration;
 
 //console.log(configuration.serverMetadata());
