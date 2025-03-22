fe0afeb906955a50875f02617d0a63ea83caba9b
jamesmoore <jamesmoore@whatever.com>
2025-03-22 07:56:52 +0000

    correct path

--- api/auth/passport-config.ts
+++ api/auth/passport-config.ts
@@ -61,7 +61,7 @@ const verify: VerifyFunctionWithRequest = (req, tokens, verified) => {
 export const passportConfig = oidcEnabled ? {
   strategy: new Strategy(oidcStrategyOptions, verify),
   middleware: (_req: Request, res: Response, next: NextFunction) => {
-    if(_req.path.startsWith('/mail.svg') || _req.path.startsWith('/status')) return next();
+    if(_req.path.startsWith('/mail.svg') || _req.path.startsWith('/api/status')) return next();
     return ensureLoggedIn('/login')(_req, res, next);
   }
 } : {
