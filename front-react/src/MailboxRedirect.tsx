19216c2ff36175841f2be691fda5a1919c1c7f34
James Moore <jamesmoore@users.noreply.github.com>
2025-10-12 07:54:37 +0100

    navigate to first unread (#565)
    * navigate to first unread

---
+++ front-react/src/MailboxRedirect.tsx
@@ -0,0 +1,36 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import useAddressResponse from "./useAddressResponse";
+import useUnreadCounts from "./useUnreadCounts";
+import { AddressesResponse } from "./models/addresses-response";
+import { UnreadCount } from "./models/unread-count";
+
+function getFirstUnreadOrDefault(
+  unreadCounts?: UnreadCount[],
+  addressResponse?: AddressesResponse
+): string | undefined {
+  const firstUnread = unreadCounts?.find(p => p.unread > 0);
+  if (firstUnread) return firstUnread.recipient;
+  return addressResponse?.addresses[0]?.addr;
+}
+
+function MailboxRedirect() {
+  const navigate = useNavigate();
+  const { data: addressResponse, isLoading: isAddressesLoading } = useAddressResponse();
+  const { data: unreadCounts, isLoading: isUnreadLoading } = useUnreadCounts();
+
+  useEffect(() => {
+    if (isAddressesLoading || isUnreadLoading) return;
+
+    if (!addressResponse || !unreadCounts) return;
+
+    const firstUnread = getFirstUnreadOrDefault(unreadCounts, addressResponse);
+    if (firstUnread) {
+      navigate(`/inbox/${firstUnread}`, { replace: true });
+    }
+  }, [unreadCounts, addressResponse, isAddressesLoading, isUnreadLoading, navigate]);
+
+  return null;
+}
+
+export default MailboxRedirect;
