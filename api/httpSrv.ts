fa51b3a21832d8a0dab7ab781d2d94b1dbbdc73d
jamesmoore <jamesmoore@github.com>
2025-12-06 08:17:47 +0000

    do not protect the splat static content endpoint

--- api/httpSrv.ts
+++ api/httpSrv.ts
@@ -69,7 +69,7 @@ export class HttpServer {
 
 		// catch-all handler for react router. This is needed so that urls that are refreshed activate the react router. The alternative 302 redirect to / would break that.
 		// https://expressjs.com/en/guide/migrating-5.html#path-syntax
-		app.get('/{*splat}', authMiddleware, (_req, res) => {
+		app.get('/{*splat}', (_req, res) => {
 			res.sendFile(join(__dirname, staticContentPath, 'index.html'), (err) => {
 				if (err) {
 					res.status(500).send(err)
