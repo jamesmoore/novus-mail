0eb5bec969c05018c8c2da16311d0260acfc4583
jamesmoore <jamesmoore@github.com>
2025-12-07 15:13:17 +0000

    log isrefetching

--- front-react/src/Root.tsx
+++ front-react/src/Root.tsx
@@ -3,11 +3,11 @@ import Login from "./Login";
 import MailboxRedirect from "./MailboxRedirect";
 
 function Root() {
-    const { data: user, isLoading: isUserLoading } = useUser();
+    const { data: user, isLoading: isUserLoading, isRefetching  } = useUser();
 
     const notLoggedIn = !user || !user.isAuthenticated && user.requiresAuth;
 
-    console.log("Root/Needs login: ", notLoggedIn);
+    console.log("Root/Needs login: ", notLoggedIn, " isRefecting: " + isRefetching);
 
     return notLoggedIn ?
         <Login strategy={user?.strategy ?? "..."} loading={isUserLoading} /> :
