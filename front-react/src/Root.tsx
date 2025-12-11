869d4dbac2e1ff30b0dddc3a61fbb480fe1a0dc6
jamesmoore <jamesmoore@github.com>
2025-12-11 22:19:11 +0000

    improve root logic

--- front-react/src/Root.tsx
+++ front-react/src/Root.tsx
@@ -5,11 +5,21 @@ import MailboxRedirect from "./MailboxRedirect";
 function Root() {
     const { data: user, isLoading, isRefetching } = useUser();
 
-    const notLoggedIn = !user || isLoading || isRefetching || !user.isAuthenticated && user.requiresAuth;
+    // True only when we have no user info AND React Query is fetching/refetching
+    const loadingInitial = isLoading || (!user && isRefetching);
 
-    return notLoggedIn ?
-        <Login strategy={user?.strategy ?? "..."} loading={isLoading || isRefetching} /> :
-        <MailboxRedirect />;
+    if (loadingInitial) {
+        return <Login loading={true} strategy="..." />;
+    }
+
+    const authenticated = user?.isAuthenticated;
+    const requiresAuth = user?.requiresAuth;
+
+    if (!authenticated && requiresAuth) {
+        return <Login strategy={user.strategy} loading={false} />;
+    }
+
+    return <MailboxRedirect />;
 }
 
 export default Root;
