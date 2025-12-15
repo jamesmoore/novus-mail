64e9da20f31abdec6cd7491874708bfe0609f88a
jamesmoore <jamesmoore@github.com>
2025-12-15 07:45:47 +0000

    add more tailwind styles

--- front-react/src/Manage.tsx
+++ front-react/src/Manage.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useMemo, useState } from 'react';
 import { addAddress as apiAddAddress, deleteAddress as apiDeleteAddress, getAddress, logout, updateAddress } from './api-client';
-import Grid from '@mui/material/Grid';
 import useAddressResponse from './useAddressResponse';
 import useDomain from './useDomain';
 import { enqueueSnackbar, SnackbarProvider } from 'notistack';
@@ -117,23 +116,16 @@ function Manage() {
     const paperClassName = "rounded-sm paper-background shadow-sm";
 
     return (
-        <Grid>
-            {/*
-            <h1 className="text-3xl font-bold underline">
-                Hello world!
-            </h1>
-            */}
+        <>
             <SnackbarProvider />
             {
                 user && <div className={paperClassName}>
-                    <Grid container mb={1} ml={1} mr={1} p={1}>
-                        <Grid size={{ xs: 12, md: 3 }} display={'flex'} alignItems={'center'} sx={{
-                            mb: { xs: 1, md: 0 }
-                        }} >
+                    <div className='flex flex-wrap m-1 ml-2 p-1'>
+                        <div className='flex items-center w-full md:w-3/12'>
                             User
-                        </Grid>
-                        <Grid container size={{ xs: 12, md: 9 }} flexDirection={'row'} >
-                            <Grid container direction="row" alignItems={'center'} flex="0 0 auto" size={{ xs: 12, md: 10 }} columnGap={1}>
+                        </div>
+                        <div className='flex w-full md:w-9/12'  >
+                            <div className='flex items-center w-full md:w-10/12 gap-1' >
                                 {user.picture ?
                                     <Avatar>
                                         <AvatarImage
@@ -143,71 +135,63 @@ function Manage() {
                                     </Avatar>
                                     : <User />}
                                 {user.email ?? 'Anon'} ({user.strategy})
-                            </Grid>
-                            <Grid display={'flex'} size={{ xs: 12, md: 2 }} justifyContent={'right'} sx={{
-                                mt: { xs: 2, md: 0 }
-                            }}>
+                            </div>
+                            <div className='flex w-full md:w-2/12 justify-end' >
                                 <Button disabled={!user.requiresAuth} onClick={doLogout}>
                                     <LogOut />Logout
                                 </Button>
-                            </Grid>
-                        </Grid>
-                    </Grid>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             }
             <div className={paperClassName}>
-                <Grid container m={1} p={1}>
-                    <Grid mt={1} size={{ xs: 12, md: 3 }}>
+                <div className="flex flex-wrap m-1 ml-2 p-1">
+                    <div className='flex items-center w-full md:w-3/12'>
                         New address
-                    </Grid>
-                    <Grid container size={{ xs: 12, md: 9 }} flexDirection={'row'}>
-                        <Grid container direction="row" alignItems={'center'} flex="0 0 auto" size={{ xs: 12, md: 10 }}>
+                    </div>
+                    <div className='flex w-full md:w-9/12'>
+                        <div className='flex items-center w-full md:w-10/12 '>
                             <Input
                                 type="text"
                                 onChange={event => setNewAddressText(event.target.value)}
                                 value={newAddressText}
                                 placeholder="New address"
-                                style={{ flex: 1 }}
                                 aria-invalid={newAddressText !== '' && (isValidAddress === false || addressExists)}
                             />
                             {newAddressText !== '' && isValidAddress === false && <p className='text-muted-foreground text-red-700 text-xs'>This email is invalid.</p>}
                             {newAddressText !== '' && addressExists && <p className='text-muted-foreground text-red-700 text-xs'>Address exists.</p>}
                             @{domainName}
-                        </Grid>
-                        <Grid display={'flex'} size={{ xs: 12, md: 2 }} justifyContent={'right'} sx={{
-                            mt: { xs: 2, md: 0 }
-                        }}>
+                        </div>
+                        <div className='flex w-full md:w-2/12 justify-end' >
                             <Button disabled={newAddressText === '' || isValidAddress === false || addressExists} onClick={addAddress}>
                                 <Plus />Add
                             </Button>
-                        </Grid>
-                    </Grid>
-                </Grid>
+                        </div>
+                    </div>
+                </div>
             </div>
             {
                 (addressesResponse?.addresses?.length ?? 0) > 0 &&
                 <div className={paperClassName}>
-                    <Grid container m={1} p={1} >
-                        <Grid container mt={2} mb={2} size={{ xs: 12, md: 3 }} >
+                    <div className="flex flex-wrap m-1 ml-2 p-1 ">
+                        <div className='flex w-full md:w-3/12 mt-1'>
                             Manage addresses
-                        </Grid>
-                        <Grid container size={{ xs: 12, md: 9 }} flexDirection={'column'}>
+                        </div>
+                        <div className='flex flex-col w-full md:w-9/12'>
                             {
                                 addressesResponse && addressesResponse.addresses.map(({ addr, owner }) => (
-                                    <Grid
+                                    <div
                                         key={addr}
-                                        container
-                                        pt={1}
-                                        pb={1}
-                                        flexDirection={'row'}
+                                        className='flex flex-row pb-1'
                                         onPointerEnter={() => { setSelectedAddress(addr) }}
                                         onPointerLeave={() => { setSelectedAddress(''); setDeleteAddress(''); }}
                                     >
-                                        <Grid display={'flex'} alignItems={'center'}>
+                                        <div className='flex items-center' >
                                             <span>{addr}</span>
                                             <span style={{ opacity: 0.3 }}>@{domainName}</span>
-                                        </Grid>
-                                        <Grid display={'flex'} sx={{ marginLeft: 'auto' }} justifyContent={'right'} alignItems={'center'}>
+                                        </div>
+                                        <div className='flex items-center justify-end ml-auto gap-2' >
                                             {deleteAddress !== addr &&
                                                 <>
                                                     {owner ? 'Private' : 'Public'}
@@ -231,15 +215,15 @@ function Manage() {
                                                     </Button>
                                                 </>
                                             }
-                                        </Grid>
-                                    </Grid>
+                                        </div>
+                                    </div>
                                 ))
                             }
-                        </Grid>
-                    </Grid>
+                        </div>
+                    </div>
                 </div>
             }
-        </Grid >
+        </ >
     );
 }
 
