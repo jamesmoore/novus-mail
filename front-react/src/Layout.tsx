1e12de13f30dbb7ab671446a367ee1c7d2f0a6a3
jamesmoore <jamesmoore@github.com>
2025-12-21 23:59:43 +0000

    fix formatting

--- front-react/src/Layout.tsx
+++ front-react/src/Layout.tsx
@@ -14,21 +14,21 @@ export default function Layout({ children, topBarChildren }: LayoutProps) {
 
   return <>
     <WebSocketNotificationHandler />
-      <SidebarProvider >
-        <AppSidebar />
-        <div className="flex flex-col w-full min-w-0">
-          {topBarChildren &&
-            <div className="sticky top-0">
-              <div className="bg-sidebar p-1 pr-3">
-                {topBarChildren}
-              </div>
-              <div className="shrink-0 h-px w-full bg-sidebar-border"></div>
+    <SidebarProvider >
+      <AppSidebar />
+      <div className="flex flex-col w-full min-w-0">
+        {topBarChildren &&
+          <div className="sticky top-0">
+            <div className="bg-sidebar p-1 pr-3">
+              {topBarChildren}
             </div>
-          }
-          <div className="flex p-2 w-full flex-col gap-2 h-full" >
-            {children}
+            <div className="shrink-0 h-px w-full bg-sidebar-border"></div>
           </div>
+        }
+        <div className="flex p-2 w-full flex-col gap-2 h-full" >
+          {children}
         </div>
-      </SidebarProvider>
+      </div>
+    </SidebarProvider>
   </>
 }
