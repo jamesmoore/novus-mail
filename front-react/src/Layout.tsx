21f833915a123fa8d03c08de130c5584dc922672
jamesmoore <jamesmoore@github.com>
2025-12-21 22:28:23 +0000

    wrap layout in dvh height div

--- front-react/src/Layout.tsx
+++ front-react/src/Layout.tsx
@@ -14,21 +14,23 @@ export default function Layout({ children, topBarChildren }: LayoutProps) {
 
   return <>
     <WebSocketNotificationHandler />
-    <SidebarProvider>
-      <AppSidebar />
-      <div className="flex min-h-svh flex-col w-full min-w-0">
-        {topBarChildren &&
-          <div className="sticky top-0">
-            <div className="bg-sidebar p-1 pr-3">
-              {topBarChildren}
+    <div className="min-h-dvh">
+      <SidebarProvider>
+        <AppSidebar />
+        <div className="flex min-h-svh flex-col w-full min-w-0">
+          {topBarChildren &&
+            <div className="sticky top-0">
+              <div className="bg-sidebar p-1 pr-3">
+                {topBarChildren}
+              </div>
+              <div className="shrink-0 h-px w-full bg-sidebar-border"></div>
             </div>
-            <div className="shrink-0 h-px w-full bg-sidebar-border"></div>
+          }
+          <div className="flex p-2 w-full flex-col gap-2 h-full" >
+            {children}
           </div>
-        }
-        <div className="flex p-2 w-full flex-col gap-2 h-full" >
-          {children}
         </div>
-      </div>
-    </SidebarProvider>
+      </SidebarProvider>
+    </div>
   </>
 }
