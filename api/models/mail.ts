e4d30a9772d9d706e1d12c840f1a16fc371c988b
jamesmoore <jamesmoore@github.com>
2025-12-23 08:33:43 +0000

    separate db from routes

---
+++ api/models/mail.ts
@@ -0,0 +1,10 @@
+export interface Mail {
+    id: string;
+    sender: string;
+    sendername: string;
+    recipient: string;
+    subject: string;
+    read: number;
+    received: number;
+    deleted: number;
+}
