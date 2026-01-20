873f77a981ffdfc0efa99129157e13b94ae46e41
James Moore <jamesmoore@users.noreply.github.com>
2026-01-20 19:08:27 +0000

    respond to failure in callback (#717)

--- api/smtp/smtp-server.ts
+++ api/smtp/smtp-server.ts
@@ -15,71 +15,57 @@ export class SMTPServer {
 	public start(): void {
 		const mailHandler = this.mailHandler;
 		const opt: SMTPServerOptions = {
-
 			async onData(stream, _session, callback) {
 				try {
 					await mailHandler.handleIncomingMail(stream, _session);
+					callback(null);
 				} catch (err) {
-					console.log("Processing email error");
-					console.log(err);
+					console.log("Processing email error", err);
+					const error = new Error("Temporary failure.") as Error & { responseCode?: number };
+					error.responseCode = 451;
+					callback(error);
 				}
-				callback(null);
 			},
-
 			authOptional: true,
-
 			onConnect(_session, callback) {
-
 				return callback();
-
 			},
-		}
-
+		};
+		
 		try {
-
 			//automatically detect public / private key
 			const files = readdirSync("./data");
 			for (const fileName of files) {
 
 				const ext = extname(fileName);
 				if (ext != ".db" && ext != ".json") {
-
-					const content = readFileSync("./data/" + fileName, 'utf8');
+					const fileWithPath = "./data/" + fileName;
+					const content = readFileSync(fileWithPath, 'utf8');
 					if (content.includes("PRIVATE KEY")) {
+						console.log("Using key: ", fileWithPath);
 						opt.key = content;
 					}
 
 					if (content.includes("BEGIN CERTIFICATE")) {
+						console.log("Using cert: ", fileWithPath);
 						opt.cert = content;
 					}
-
 				}
-
 			}
 
 		} catch (err) {
-
-			console.log("read directory fail");
-			console.log(err);
-
+			console.log("read directory fail", err);
 		}
 
 		const server = new ssrv(opt);
 		server.on('error', (err) => {
-
-			console.log("SMTP server error");
-			console.log(err);
-
+			console.log("SMTP server error", err);
 		});
 
 		server.listen(this.port, () => {
-
-			console.log('smtp server running at port: ' + this.port);
-
+			console.log('smtp server running at port: ', this.port);
 		});
-
 	}
-
 }
 
 
