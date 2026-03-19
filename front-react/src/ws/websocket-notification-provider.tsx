3bf811c655f7ad0a6facb6429564fdf366a870d3
jamesmoore <jamesmoore@github.com>
2026-03-19 17:11:16 +0000

    fix vite8 react-use-websocket reference

--- front-react/src/ws/websocket-notification-provider.tsx
+++ front-react/src/ws/websocket-notification-provider.tsx
@@ -1,9 +1,10 @@
 import { FC, ReactNode } from 'react';
-import useWebSocket from 'react-use-websocket';
+import { useWebSocket } from "react-use-websocket/dist/lib/use-websocket";
 import { WebSocketMessage, WebSocketContext } from './use-websocket-notifier';
 
 export const WebSocketNotificationProvider: FC<{ children: ReactNode; }> = ({ children }) => {
-    const webSocketHook = useWebSocket<WebSocketMessage>((import.meta.env.VITE_API_BASE_URL ?? '') + '/ws', {
+    const websocketUrl = `${import.meta.env.VITE_API_BASE_URL ?? ''}/ws`;
+    const webSocketHook = useWebSocket<WebSocketMessage>(websocketUrl, {
         onOpen: () => console.debug("ws connected"),
         shouldReconnect: () => true
     });
